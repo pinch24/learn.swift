00330e5938f6b2bd325b92c6e7bb3e3ef5f97c80
Pinch24 <pinch24@gmail.com>
2021-03-14 23:51:48 +0900

    Operator

---
+++ 04. Swift 5 by Yagom/Swift 5 Programming/Operator/Operator/1. Operator/02. Arithmetic.swift
@@ -0,0 +1,16 @@
+/**
+5.1.2 산술(arithmatic) 연산자
+*/
+import Foundation
+
+func exec_5102() {
+	
+	let a = 4
+	let b = 2
+	
+	print("\(a) + \(b) = \(a + b)")
+	print("\(a) - \(b) = \(a - b)")
+	print("\(a) * \(b) = \(a * b)")
+	print("\(a) / \(b) = \(a / b)")
+	print("\(a) % \(b) = \(a % b)")
+}
