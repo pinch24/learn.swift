15df87173b6af29b960a0f8486d31562f3fc849a
mk <pinch24@gmail.com>
2021-10-31 13:30:23 +0900

    프로젝트 생성

---
+++ 03. SwiftUI Essentials by Neil Smyth/SwiftUI Essentials/Shared/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  Shared
+//
+//  Created by Bob on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
