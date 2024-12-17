f70a0e81853c0088aef299818c089d880ec53b5b
mk <pinch24@gmail.com>
2024-12-18 05:42:50 +0900

    Swift - SeeFood: CoreML

--- Udemy/AngelaYu/SeeFood/SeeFood/ViewController.swift
+++ Udemy/AngelaYu/SeeFood/SeeFood/ViewController.swift
@@ -25,6 +25,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 		if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
 			imageView.image = userPickedImage
+			guard let ciImage = CIImage(image: userPickedImage) else {
+				fatalError("Could not convert UIImage into CIImage.")
+			}
+			detect(image: ciImage)
+		}
+		
+		imagePicker.dismiss(animated: true, completion: nil)
+	}
+	
+	func detect(image: CIImage) {
+		guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else {
+			fatalError("Loading CoreML Model Fatal.")
+		}
+		
+		let request = VNCoreMLRequest(model: model) { (request, error) in
+			guard let results = request.results as? [VNClassificationObservation] else {
+				fatalError("Model failed to process image.")
+			}
+			
+			if let firstResult = results.first {
+				self.navigationItem.title = firstResult.identifier
+			}
+		}
+		
+		let handler = VNImageRequestHandler(ciImage: image)
+		
+		do {
+			try handler.perform([request])
+		}
+		catch {
+			print(error)
 		}
 	}
 	
