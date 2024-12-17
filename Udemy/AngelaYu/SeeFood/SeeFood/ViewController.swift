798b1d8722f036e580b30659024a60653c01615f
mk <pinch24@gmail.com>
2024-12-17 23:09:09 +0900

    Swift - SeeFood: CoreML

---
+++ Udemy/AngelaYu/SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by MK on 12/17/24.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	@IBOutlet weak var imageView: UIImageView!
+	
+	let imagePicker = UIImagePickerController()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		imagePicker.delegate = self
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.allowsEditing = false
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+			imageView.image = userPickedImage
+		}
+	}
+	
+	@IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+		present(imagePicker, animated: true, completion: nil)
+	}
+	
+}
